4a86c052cc9a72f713bab87eaf21a423ddc760a2
xinzT <tuxinhe152xd@gmail.com>
2026-06-11 12:42:54 +0800

    [clean]unuseless tool and result

--- source_materials/data_transformer.py
+++
@@ -1,7 +0,0 @@
-from markitdown import MarkItDown  
-md = MarkItDown()
-test_result = md.convert('group_six.pdf')
-print(test_result.text_content)
-
-with open("group_six.md", "w", encoding="utf-8") as f:  
-    f.write(test_result.markdown)  
